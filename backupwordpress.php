248d566e3752d73e615d00c86391f87d804393b6
dnutbourne <dnutbourne@updraftplus.com>
2019-04-25 12:24:55 +0100

    Switch from email address to support forums

--- backupwordpress.php
+++ backupwordpress.php
@@ -14,7 +14,7 @@ Network: true
 */
 
 /*
-Copyright 2011 - 2018 XIBO Ltd  (email : support@xibomarketing.com)
+Copyright 2011 - 2018 XIBO Ltd
 
 This program is free software; you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
